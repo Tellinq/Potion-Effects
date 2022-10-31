5954108ab3c39dd4e182c1153c097dc6c2b1094a
Tellinq <59412384+Tellinq@users.noreply.github.com>
2022-10-30 22:28:25 -0400

    Move drawAll into dedicated methods
    * Make the check if the activeEffects is not empty in shouldShow(). This will still account for the example check
    * Delete the amount in counter. This was previously to make sure the background and any other prerendered elements did not actually render when the effects were empty (especially if all effects were using an exclusion check)
    * Make dummy effects use a separate list and add on startup. Should help with performance slightly.
    
    Also made some documentation. Rough draft of each though. Will revise at a later date.
    
    Just before pushing I noticed that there's an old bug present again where the effects won't actually render until you enter the GUI editor. Probably some dumb oversight by me

--- src/main/java/tk/tellinq/potioneffects/hud/PotionEffects.java
+++ src/main/java/tk/tellinq/potioneffects/hud/PotionEffects.java
@@ -1,10 +1,10 @@
 package tk.tellinq.potioneffects.hud;
 
+import cc.polyfrost.oneconfig.events.event.InitializationEvent;
 import cc.polyfrost.oneconfig.libs.universal.UGraphics;
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
+import com.google.common.collect.ImmutableList;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.entity.player.EntityPlayer;
 import tk.tellinq.potioneffects.config.PotionEffectsConfig;
 import cc.polyfrost.oneconfig.config.annotations.*;
 import cc.polyfrost.oneconfig.config.core.OneColor;
@@ -77,7 +77,6 @@ public class PotionEffects extends BasicHud {
     @Exclude public final int ICON_SIZE = 18;
     @Exclude private final ResourceLocation EFFECTS_RESOURCE = new ResourceLocation("textures/gui/container/inventory.png");
     @Exclude public final Minecraft mc = UMinecraft.getMinecraft();
-    @Exclude public final EntityPlayerSP player = mc.thePlayer;
     @Exclude public final FontRenderer fontRenderer = mc.fontRendererObj;
     @Exclude public Map<Integer, EffectConfig> effectMap =
             new ImmutableMap.Builder<Integer, EffectConfig>()
@@ -107,14 +106,21 @@ public class PotionEffects extends BasicHud {
     private float width = 0f;
     private float height = 0f;
     @Exclude private int ticks = 0;
-    @Exclude private List<PotionEffect> potionEffects;
-    @Exclude private boolean dummy = false;
+    @Exclude private List<PotionEffect> activeEffects = new ArrayList<>();
+    @Exclude private List<PotionEffect> currentEffects = new ArrayList<>();
+    @Exclude private final List<PotionEffect> dummyEffects = new ArrayList<>();
 
     public PotionEffects() {
         super(true, 0, 0, 1, false, false, 0, 0, 0, new OneColor(0, 0, 0, 120), false, 2, new OneColor(0, 0, 0));
         EventManager.INSTANCE.register(this);
     }
 
+    /**
+     * On every tick, this tick counter will add up every tick.
+     * <br>
+     * Primarily used if a component should blink.
+     * @param event
+     */
     @Subscribe
     private void onTick(TickEvent event) {
         if (event.stage == Stage.START) {
@@ -122,48 +128,54 @@ public class PotionEffects extends BasicHud {
         }
     }
 
-    @Override
-    public void drawAll(UMatrixStack matrices, boolean example) {
-        this.potionEffects = new ArrayList<>();
-        if (this.player != null) {
-            this.potionEffects.addAll(this.player.getActivePotionEffects());
-        }
-        this.dummy = example && this.potionEffects.isEmpty();
-        if (this.dummy) {
-            this.potionEffects.add(new PotionEffect(Potion.moveSpeed.id, 1200, 1));
-            this.potionEffects.add(new PotionEffect(Potion.damageBoost.id, 30, 3));
-        }
-
-        int amount = 0;
-        for (PotionEffect effect : this.potionEffects) {
-            EffectConfig effectSetting = getEffectSetting(effect);
-            EffectConfig oExclusion = useOverride(effectSetting, effectSetting.overrideExclusion);
-            if (excludePotions(oExclusion, effect)) {
-                if (!example) {
-                    continue;
-                }
-            }
-            amount++;
-        }
-
-        if (amount == 0) {
-            return;
-        }
-
-        super.drawAll(matrices, example);
+    /**
+     * On Forge's initialization, add the two example/dummy effects.
+     * <br>
+     * Historically, in CheatBreaker, current versions of Lunar Client, and in early versions of the mod, this used to constantly be re-added every partial tick as the field used to make an array list was made every partial tick as well.
+     */
+    @Subscribe
+    private void onInitialization(InitializationEvent event) {
+        this.dummyEffects.add(new PotionEffect(Potion.moveSpeed.id, 1200, 1));
+        this.dummyEffects.add(new PotionEffect(Potion.damageBoost.id, 30, 3));
     }
 
+    /**
+     * @return True if the active player effects are not empty and the basic HUD conditions are met.
+     */
+    @Override
+    protected boolean shouldShow() {
+        return !this.activeEffects.isEmpty() && super.shouldShow();
+    }
+
+    /**
+     * Draws the current effects, except for any marked as excluded.
+     * @param matrices The UMatrixStack used for rendering in higher versions
+     * @param x        Top left x-coordinate of the hud
+     * @param y        Top left y-coordinate of the hud
+     * @param scale    Scale of the hud
+     * @param example  If the HUD is being rendered in example form
+     */
     @Override
     protected void draw(UMatrixStack matrices, float x, float y, float scale, boolean example) {
         UGraphics.disableLighting();
 
-        this.softEffects(this.potionEffects);
+        /*
+        In my opinion, this should only be set every time the potion effect gets updated. I haven't looked into making events for OneConfig, but I will later on.
+        If anyone wants to take a look at implementing this before I decide to, this is what I did for CheatBreaker: https://imgur.com/OOhV7H6 https://imgur.com/2BJAanz
+         */
+        if (this.mc.thePlayer != null) {
+            this.activeEffects = ImmutableList.copyOf(this.mc.thePlayer.getActivePotionEffects());
+        }
+
+        this.currentEffects = this.activeEffects.isEmpty() ? this.dummyEffects : this.activeEffects;
+
+        this.softEffects(this.currentEffects);
 
         float yOff = 0;
         float xOff = 0;
         int yAmt = (int) (this.ICON_SIZE + this.verticalSpacing);
 
-        this.height = (this.potionEffects.size() * yAmt) - this.verticalSpacing;
+        this.height = (this.currentEffects.size() * yAmt) - this.verticalSpacing;
         this.width = 0f;
 
         UGraphics.GL.pushMatrix();
@@ -171,7 +183,7 @@ public class PotionEffects extends BasicHud {
         // I would make this a method myself, but I find it pointless since it's just two lines of code and making a new one just seems silly unless I was repeatedly calling it for multiple HUD mods.
         UGraphics.GL.scale(scale, scale, 1);
         UGraphics.GL.translate(x / scale, y / scale, 1);
-        for (PotionEffect effect : this.potionEffects) {
+        for (PotionEffect effect : this.currentEffects) {
             EffectConfig effectSetting = getEffectSetting(effect);
             // I wish there was a more efficient way of doing this...
             EffectConfig oComponent = useOverride(effectSetting, effectSetting.overrideComponent);
@@ -189,8 +201,11 @@ public class PotionEffects extends BasicHud {
                     continue;
                 }
             }
+
             Potion potion = Potion.potionTypes[effect.getPotionID()];
-            if (!potion.shouldRender(effect)) continue;
+            if (!potion.shouldRender(effect)) {
+                continue;
+            }
 
 
             if (oComponent.icon) {
@@ -200,7 +215,7 @@ public class PotionEffects extends BasicHud {
                 if (this.horizontalAlignment == 2) {
                     iconX = this.width - ICON_SIZE;
                 }
-                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectIconBlink, effect.getDuration())) {
+                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectIconBlink, effect.getDuration(), example)) {
                     this.mc.ingameGUI.drawTexturedModalRect(iconX, yOff, potion.getStatusIconIndex() % 8 * 18, 198 + potion.getStatusIconIndex() / 8 * 18, 18, 18);
                 }
                 xOff = this.ICON_SIZE;
@@ -214,9 +229,19 @@ public class PotionEffects extends BasicHud {
 
 
                 StringBuilder titleSb = new StringBuilder();
-                if (oFormatting.boldEffectName) titleSb.append(EnumChatFormatting.BOLD);
-                if (oFormatting.italicEffectName) titleSb.append(EnumChatFormatting.ITALIC);
-                if (oFormatting.underlineEffectName) titleSb.append(EnumChatFormatting.UNDERLINE);
+                // I really hope there's a more efficient way of setting this up...
+                if (oFormatting.boldEffectName) {
+                    titleSb.append(EnumChatFormatting.BOLD);
+                }
+
+                if (oFormatting.italicEffectName) {
+                    titleSb.append(EnumChatFormatting.ITALIC);
+                }
+
+                if (oFormatting.underlineEffectName) {
+                    titleSb.append(EnumChatFormatting.UNDERLINE);
+                }
+
                 if (oFormatting.customName.isEmpty()) {
                     titleSb.append(I18n.format(potion.getName()));
                 } else {
@@ -253,7 +278,7 @@ public class PotionEffects extends BasicHud {
                     case 2:
                         titleX = this.width - xOff;
                 }
-                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectNameBlink, effect.getDuration())) {
+                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectNameBlink, effect.getDuration(), example)) {
                     RenderManager.drawScaledString(builtTitle, titleX, titleY, getColor(oColor.nameColor.getRGB(), excluded), RenderManager.TextType.toType(oFormatting.textType), 1);
                 }
 
@@ -264,9 +289,19 @@ public class PotionEffects extends BasicHud {
                 if (oComponent.icon) xOff = (this.ICON_SIZE + 4);
 
                 StringBuilder timeSb = new StringBuilder();
-                if (oFormatting.boldDuration) timeSb.append(EnumChatFormatting.BOLD);
-                if (oFormatting.italicDuration) timeSb.append(EnumChatFormatting.ITALIC);
-                if (oFormatting.underlineDuration) timeSb.append(EnumChatFormatting.UNDERLINE);
+                // I really hope there's a more efficient way of setting this up...
+                if (oFormatting.boldDuration) {
+                    timeSb.append(EnumChatFormatting.BOLD);
+                }
+
+                if (oFormatting.italicDuration) {
+                    timeSb.append(EnumChatFormatting.ITALIC);
+                }
+
+                if (oFormatting.underlineDuration) {
+                    timeSb.append(EnumChatFormatting.UNDERLINE);
+                }
+
                 if (effect.getIsPotionDurationMax()) {
                     timeSb.append(oFormatting.maxDurationString);
                 } else {
@@ -282,16 +317,20 @@ public class PotionEffects extends BasicHud {
                     }
 
                 }
+
                 String builtTime = timeSb.toString();
 
                 int timeWidth = this.fontRenderer.getStringWidth(builtTime);
                 this.width = Math.max(width, xOff + timeWidth);
 
                 float timeX = xOff;
-
                 float timeY = yOff + this.fontRenderer.FONT_HEIGHT + 1;
-                if (!oComponent.effectName) timeY -= this.fontRenderer.FONT_HEIGHT / 2f + 0.5f;
-                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectDurationBlink, effect.getDuration())) {
+
+                if (!oComponent.effectName) {
+                    timeY -= this.fontRenderer.FONT_HEIGHT / 2f + 0.5f;
+                }
+
+                if (showEffectDuringBlink(oBlinking, oBlinking.makeEffectDurationBlink, effect.getDuration(), example)) {
                     switch (this.horizontalAlignment) {
                         case 0:
                             timeX = xOff;
@@ -311,6 +350,13 @@ public class PotionEffects extends BasicHud {
         UGraphics.GL.popMatrix();
     }
 
+    /**
+     * Sorts all the current potion effects based off what sorting method the user set
+     * 1: Sorts alphabetically by name only
+     * 2: Sorts alphabetically based off duration.
+     * 3: Sorts alphabetically based off amplifier.
+     * Optionally, the entire list can get reversed if the user enables Vertical Sorting.
+     */
     public void softEffects(List<PotionEffect> potionEffects) {
         switch (this.sortingMethod) {
             case 1:
@@ -323,12 +369,26 @@ public class PotionEffects extends BasicHud {
                 potionEffects.sort(Comparator.comparingInt(PotionEffect::getAmplifier));
                 Collections.reverse(potionEffects);
         }
-        if (this.verticalSorting) Collections.reverse(potionEffects);
+
+        if (this.verticalSorting) {
+            Collections.reverse(potionEffects);
+        }
     }
 
-    private boolean showEffectDuringBlink(EffectConfig effectConfig, boolean blinkType, float duration) {
-        if (effectConfig.blink && blinkType) {
-            if (effectConfig.syncBlinking || this.dummy) {
+    /**
+     * @param effectConfig The current effect's configuration (global or effect specific)
+     * @param makeComponentBlink If the set component should blink
+     * @param duration The effect's duration
+     * @param example If the user is currently in the HUD editor
+     * @return False if either:
+     * <br>
+     * - If synced or if the example effects are running: Depending on the amount of counted ticks and the "speed" slider amount set, those two main factors determine if the element set should show while blinking.
+     * <br>
+     * - Depending on the effect's duration, that will determine if the effect should blink (will explain in depth later)
+     */
+    private boolean showEffectDuringBlink(EffectConfig effectConfig, boolean makeComponentBlink, float duration, boolean example) {
+        if (effectConfig.blink && makeComponentBlink) {
+            if (effectConfig.syncBlinking || (example && this.activeEffects.isEmpty())) {
                 float blinkSpeed = effectConfig.blinkSpeed / 3.0f;
                 if (duration <= effectConfig.blinkDuration * 20.0F) {
                     if (this.ticks > blinkSpeed * 2) this.ticks = 0;
@@ -341,6 +401,10 @@ public class PotionEffects extends BasicHud {
         return true;
     }
 
+    /**
+     * @param effect The current potion effect
+     * @return The specific effect config's based off the effectMap if overridden, or the global configuration if not overridden.
+     */
     public EffectConfig getEffectSetting(PotionEffect effect) {
         for (Map.Entry<Integer, EffectConfig> entry : this.effectMap.entrySet()) {
             if (effect.getPotionID() == entry.getKey()) {
@@ -350,11 +414,21 @@ public class PotionEffects extends BasicHud {
         return PotionEffectsConfig.global;
     }
 
+    /**
+     * @param effectSetting The current effect setting.
+     * @param overrideBoolean The override check based off the category's override check.
+     * @return True if the category's override check is enabled.
+     */
     public EffectConfig useOverride(EffectConfig effectSetting, boolean overrideBoolean) {
-        if (overrideBoolean) return effectSetting;
-        else return PotionEffectsConfig.global;
+        return overrideBoolean ? effectSetting : PotionEffectsConfig.global;
     }
 
+    /**
+     * Exclude the current effect depending on exclusion configuration
+     * @param effectSetting The current effect's setting.
+     * @param effect The current potion effect
+     * @return True if one of the exclusion conditions is set to true
+     */
     private boolean excludePotions(EffectConfig effectSetting, PotionEffect effect) {
         if (effectSetting.excludePermanentEffects && effect.getIsPotionDurationMax()) return true;
         if (this.excludeBulk(effectSetting.ambientExclusionRule, effect.getIsAmbient())) return true;
@@ -364,6 +438,16 @@ public class PotionEffects extends BasicHud {
         return effectSetting.exclude;
     }
 
+    /**
+     * Explanation is not very well explained. Will be revised.
+     * @param rule The configuration rule
+     * @param currentValue The current effect's value
+     * @return True depending on the config's rule
+     * <br>
+     * 1: Matches the effect's value
+     * <br>
+     * 2: Does not match the effect's value
+     */
     protected boolean excludeBulk(int rule, boolean currentValue) {
         switch (rule) {
             case 1: return currentValue;
@@ -372,6 +456,21 @@ public class PotionEffects extends BasicHud {
         }
     }
 
+    /**
+     *
+     * @param rule The configuration rule
+     * @param currentValue The current effect's value
+     * @param threshold The threshold amount
+     * @return True depending on the rule number set
+     * <br>
+     * 1: Current value exceeds threshold
+     * <br>
+     * 2: Current value is below threshold
+     * <br>
+     * 3: Current value matches threshold
+     * <br>
+     * 4: Current value does not match threshold
+     */
     protected boolean excludeArrayOptions(int rule, int currentValue, float threshold) {
         switch (rule) {
             case 1: return currentValue > threshold;
@@ -382,22 +481,40 @@ public class PotionEffects extends BasicHud {
         }
     }
 
+    /**
+     * @param color The color given
+     * @param excluded If the effect is excluded
+     * @return The given color or half of the color's opacity if the effect is excluded.
+     */
     private int getColor(int color, boolean excluded) {
         int opacity = color >> 24 & 0xFF;
         opacity = (int)((float)opacity * (excluded ? 0.5F : 1.0F));
         return color & 0xFFFFFF | opacity << 24;
     }
 
+    /**
+     * @param scale   Scale of the hud
+     * @param example If the HUD is being rendered in example form
+     * @return The width of the HUD multiplied by the HUD's scale
+     */
     @Override
     protected float getWidth(float scale, boolean example) {
         return this.width * scale;
     }
 
+    /**
+     * @param scale   Scale of the hud
+     * @param example If the HUD is being rendered in example form
+     * @return The height of the HUD multiplied by the HUD's scale
+     */
     @Override
     protected float getHeight(float scale, boolean example) {
         return this.height * scale;
     }
 
+    /**
+     * All the individual config settings.
+     */
     public static class EffectConfig {
         @Switch(
                 name = "Override",
