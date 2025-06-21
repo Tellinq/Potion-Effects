a62648a98ad37ea55c41fb60ed799c2a47a619ee
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-06-21 09:05:18 -0400

    Update core project

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.6.21" apply false
-    id("cc.polyfrost.multi-version.root")
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    kotlin("jvm") version "2.0.10" apply false
+    id("org.polyfrost.multi-version.root")
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 
 preprocess {
-    "1.8.9-forge"(10809, "srg") {}
+    "1.12.2-forge"(11202, "srg") {
+        "1.8.9-forge"(10809, "srg")
+    }
 }
